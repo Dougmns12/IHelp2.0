7fb9f670b3da919b71c3fd9d9407218eef35614b
Douglas Nunes <douglasnunes5@hotmail.com>
2019-10-14 12:08:02 -0300

    Apagando algumas coisas

--- js/login.js
+++ js/login.js
@@ -3,10 +3,10 @@ var btnConta = document.querySelector("#conta");
 
 var body = document.querySelector("body");
 
-btnEntrar.addEventListener("click", function () {
+btnEntrar.addEventListener("click", function entrar () {
 	body.className = "entrar-js";
 })
 
-btnConta.addEventListener("click", function() {
+btnConta.addEventListener("click", function cadastrar () {
 	body.className = "cadastrar-js"
 })
